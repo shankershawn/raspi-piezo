c8d17e87bd010dac2fd0c0fc97a22a6210abde37
Shankarsan Ganai <shankershawn@gmail.com>
2021-08-23 01:13:47 +0530

    Changed mode and added conditional logging

--- alarms/alarmMain.py
+++ alarms/alarmMain.py
@@ -1,19 +1,23 @@
 from time import sleep
 from RPi import GPIO
+
 import batteryAlarm
 
 
 def process_main_alarm():
     try:
-        GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(29, GPIO.OUT, initial=GPIO.LOW)
+        prev_pin_state = -1
         while True:
             pin_state = batteryAlarm.get_pin_state("poom", "tzOsQ7es", 40)
-            print(f"Pin state: {pin_state}")
+            if prev_pin_state != pin_state:
+                print(f"Pin state: {pin_state}")
             GPIO.output(29, pin_state)
             sleep(2)
             GPIO.output(29, GPIO.LOW)
             sleep(2)
+            prev_pin_state = pin_state
     except KeyboardInterrupt:
         print("Exiting")
     finally:
